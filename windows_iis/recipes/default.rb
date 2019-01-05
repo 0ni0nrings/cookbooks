74487ae9503b7377ebeaa6986111ff51d712f05e
Vagrant <strike3test@hotmail.com>
2019-01-06 12:05:47 +1300

    new cookbook

---
+++ windows_iis/recipes/default.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook:: windows_iis
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+=begin
+# Default way to Install IIS but this uses DISM instead of PowerShell
+# DISM / Get-WindowsOptionalFeature
+%w{Web-Server Web-WebServer}.each do |feature|
+  windows_feature feature do
+    action :install
+  end
+end
+
+service 'w3svc' do
+  action [:start, :enable]
+end
+=end
+
+# PowerShell way to do Install IIS
+powershell_script 'Install IIS' do
+  code <<-EOH
+  $Features = @('Web-Server', 'Web-WebServer')
+  ForEach-Object -InputObject $Features | Add-WindowsFeature $_
+  EOH
+  guard_interpreter :powershell_script
+  not_if "(ForEach-Object -InputObject ($Features) {(Get-WindowsFeature $_).Installed})"
+end
+
+service 'w3svc' do
+  action [:start, :enable]
+end
